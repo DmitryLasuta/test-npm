012f1ce2926724a3926626f51f3f8788eb542284
Дмитрий Ласута <dmitriy.lasuta@uptrade.ru>
2025-11-27 17:02:44 +0300

    выкинул лишнее (:

--- apps/playground/src/providers/mantine.tsx
+++
@@ -1,6 +0,0 @@
-import { uptradeTheme } from '@tests-npm-deploy/core';
-import { MantineProvider } from '@mantine/core';
-
-export function MantineAppProvider({ children }: { children: React.ReactNode }) {
-  return <MantineProvider theme={uptradeTheme}>{children}</MantineProvider>;
-}
